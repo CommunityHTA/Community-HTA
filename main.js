ff07c6ec623336362d78140be3d122657c9ccd84
CommunityHTA <communityhta@gmail.com>
2025-03-07 20:45:19 +0000

    simulacao login user100

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginButton = document.querySelector(".login-button");
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.querySelector("input[type='password']");
 
-    loginButton.addEventListener("click", function () {
-        const username = document.getElementById("username").value;
-        const password = document.querySelector("input[type='password']").value;
+    function handleLogin() {
+        const username = usernameInput.value;
+        const password = passwordInput.value;
 
         if (username === "user100" && password === "user100") {
-            // Login bem-sucedido, redireciona para outra página
-            window.location.href = "selectApp.html";
+            // Login bem-sucedido, redireciona para a página dashboard.html
+            window.location.href = "dashboard.html";
         } else {
             // Exibe um erro se o login falhar
             alert("Usuário ou senha incorretos!");
+        }
+    }
+
+    // Evento de clique no botão de login
+    loginButton.addEventListener("click", handleLogin);
+
+    // Evento para detectar pressionamento da tecla "Enter"
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            handleLogin();
         }
     });
 });
